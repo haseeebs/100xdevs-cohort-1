1e41b439f82c50d628c762e8519f00254fc8b153
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-08 07:24:52 +0530

    Add function to print a pyramid pattern in JavaScript.

--- Index.js
+++ Index.js
@@ -8,4 +8,30 @@ function doArthmetic(first, second, arthmeticFunction) {
 
 const result = doArthmetic(5, 5, sum);
 
-console.log(result)
+
+
+const createPattern = (numberOfrows) => {
+    let starts = ''
+    for(let i = 0; i < numberOfrows; i++){
+        starts = '*' + starts
+        console.log(starts);
+    }
+}
+
+function printPyramid(height) {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        // Adding spaces before the stars
+        for (let j = 0; j < height - i; j++) {
+            row += ' ';
+        }
+        // Adding stars
+        for (let k = 0; k < 2 * i - 1; k++) {
+            row += '*';
+        }
+        console.log(row);
+    }
+};
+
+printPyramid(5);
+createPattern(6);

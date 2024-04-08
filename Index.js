1ac4d7bdac5dbc7d11154992ca981b13cc80bd3c
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-08 07:48:41 +0530

    Add functions to print a pattern of asterisks in JavaScript.

--- Index.js
+++ Index.js
@@ -1,37 +1,26 @@
-const sum = (firstNumber, secondNumber) => (
-    firstNumber + secondNumber
-)
-
-function doArthmetic(first, second, arthmeticFunction) {
-    return arthmeticFunction(first, second);
+const printSingleFullRow = (number) => {
+    let row = '';
+    for (i = 0; i < number; i++) {
+        row = row + '*'
+    }
+    console.log(row);
 }
 
-const result = doArthmetic(5, 5, sum);
-
-
-
-const createPattern = (numberOfrows) => {
-    let starts = ''
-    for(let i = 0; i < numberOfrows; i++){
-        starts = '*' + starts
-        console.log(starts);
+const printGapBetweenStars = (number) => {
+    let row = '*'
+    for (let i = 0; i < number - 2; i++) {
+        row += ' ';
     }
+    row += '*'
+    console.log(row);
 }
 
-function printPyramid(height) {
-    for (let i = 1; i <= height; i++) {
-        let row = '';
-        // Adding spaces before the stars
-        for (let j = 0; j < height - i; j++) {
-            row += ' ';
+const printThem = (number) => {
+        printSingleFullRow(number)
+        for (j = 0; j < number - 2; j++) {
+            printGapBetweenStars(number)
         }
-        // Adding stars
-        for (let k = 0; k < 2 * i - 1; k++) {
-            row += '*';
-        }
-        console.log(row);
-    }
-};
+        printSingleFullRow(number)
+}
 
-printPyramid(5);
-createPattern(6);
+printThem(10)

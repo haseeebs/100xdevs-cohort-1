d45a77878d0499dc3c43d54322034463b9221d1a
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-10 14:14:24 +0530

    Implement todo list management frontend and backend integration.

--- Index.js
+++ Index.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'), (err) => {
+        if (err) throw err;
+    })
+});
+
+app.get('/todos', (req, res) => {
     fs.readFile('todo.json', 'utf-8', (err, data) => {
         if (err) throw err;
-        const jsonData = JSON.parse(data);
-        res.json(jsonData);
+        const todoData = JSON.parse(data);
+        res.json(todoData);
     });
 });
 
@@ -36,6 +43,29 @@ app.post('/', (req, res) => {
         })
         id++;
         res.status(200).json(todoObj);
+    });
+})
+
+app.delete('/todo/:id', (req, res) => {
+    const deleteId = parseInt(req.params.id);
+    fs.readFile('todo.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+
+        const todoData = JSON.parse(data)
+
+        const filterTodos = todoData.filter(todo => todo.id !== deleteId);
+
+        if (filterTodos.length !== todoData.length) {
+            fs.writeFile('todo.json', JSON.stringify(filterTodos), (err) => {
+                if (err) throw err
+
+                console.log(`Todo with ID ${deleteId} has been deleted.`);
+                res.sendStatus(200);
+            })
+        } else {
+            // No todo was deleted
+            res.status(404).send("Todo not found");
+        }
     });
 })
 

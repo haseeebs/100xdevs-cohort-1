2f07e0f09517766ca0b2e235e1e4e04028c1f308
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-10 08:25:08 +0530

    Implement basic todo list management API with Express.js.

--- Index.js
+++ Index.js
@@ -1,27 +1,44 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-const PORT = 2127;
 
 app.use(express.json());
 
-const middleware = (req, res, next) => {
-    console.log('Middleware gets called ',req.body);
-    next();
-};
-
-app.use(middleware);
-
 app.get('/', (req, res) => {
-    res.json({
-        name: 'haseeb',
-        age: 21
+    fs.readFile('todo.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
     });
 });
 
+let id = 1;
 app.post('/', (req, res) => {
-    res.send('This is post request...');
-});
+    
+    fs.readFile('todo.json', 'utf-8' , (err , data) => {
+        if(err) throw err;
 
-app.listen(PORT, () => {
-    console.log(`Listning on port ${PORT}`);
+        const todoData = JSON.parse(data);
+        
+        const todoObj = {
+            id: id,
+            title: req.body.title,
+            description: req.body.description
+        };
+
+        todoData.push(todoObj);
+
+        fs.writeFile('todo.json', JSON.stringify(todoData), (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(`Your data has been successfully stored to the todo.json file...`);
+        })
+        id++;
+        res.status(200).json(todoObj);
+    });
 })
+
+app.listen(3000, () => {
+    console.log(`It's listning on port ${3000}`);
+});

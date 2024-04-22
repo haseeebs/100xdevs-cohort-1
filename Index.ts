b2367194e0f962b6d4dc1dcdd7401116ced19930
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-23 05:15:13 +0530

    Starting Typescript.

---
+++ Index.ts
@@ -0,0 +1,26 @@
+// TypeScript
+/**
+ * This function calculates the sum of two numbers.
+ *
+ * @param {number} a - The first number to be added.
+ * @param {number} b - The second number to be added.
+ *
+ * @returns {number} The sum of a and b.
+ */
+function calculateSum(a: number, b: number): number {
+  // Add the two numbers and return the result.
+  return a + b;
+}
+
+// C++
+
+// /**
+//  * Calculates the sum of two integers.
+//  * 
+//  * @param {int} a The first integer.
+//  * @param {int} b The second integer.
+//  * @returns {int} The sum of the two integers.
+//  */
+// int calculateSum(int a, int b){
+//     return a + b;
+// }

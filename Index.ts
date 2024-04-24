10cc72f3ca5197488cd3931fbcfe0e6266382af6
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-24 15:26:55 +0530

    Brush up Typescript's functionalities.

--- Index.ts
+++ Index.ts
@@ -1,26 +1,94 @@
-// TypeScript
-/**
- * This function calculates the sum of two numbers.
- *
- * @param {number} a - The first number to be added.
- * @param {number} b - The second number to be added.
- *
- * @returns {number} The sum of a and b.
- */
-function calculateSum(a: number, b: number): number {
-  // Add the two numbers and return the result.
-  return a + b;
+function calculator(first: number, second: number, type: string): number {
+  switch (type) {
+    case "add":
+      return first + second;
+    case "mul":
+      return first * second;
+    case "div":
+      return first / second;
+    case "sub":
+      return first - second;
+    default:
+      throw new Error("Invalid operation type");
+  }
 }
 
-// C++
+const result = calculator(5, 10, "mul");
+console.log(result);
 
-// /**
-//  * Calculates the sum of two integers.
-//  * 
-//  * @param {int} a The first integer.
-//  * @param {int} b The second integer.
-//  * @returns {int} The sum of the two integers.
-//  */
-// int calculateSum(int a, int b){
-//     return a + b;
-// }
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// Instrad of this we declare enums
+
+const enum Size {
+  small = 1,
+  medium = 2,
+  large = 3,
+}
+let mySize: Size = Size.medium;
+console.log(mySize);
+
+// interface
+
+interface Person {
+  name: String;
+  age: Number;
+}
+
+function salam(person: Person): string {
+  return `Assalamu-alaikum habibi...welcome to dubai ${person.name} you are finally here...So you're now ${person.age}`;
+}
+
+const result2 = salam({ name: "haseeb", age: 23 });
+console.log(result2);
+
+// Type alias
+// Union types
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === "number") {
+    // Type narrowing
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+
+// Define two interfaces representing diffrent aspects of an object
+
+interface Car {
+  brand: string;
+  model: string;
+  year: string;
+  sunroof?: boolean
+}
+
+interface Electric {
+  batteryCapacity: number;
+  chargingTime: string;
+}
+
+// Define a new type that combines both Car and Electric interfaces
+type ElectricCar = Car & Electric;
+
+function teslaCar(car: ElectricCar) {
+  console.log(car);
+}
+
+teslaCar({
+  brand: "Tesla",
+  model: "Model S",
+  year: "2024",
+  batteryCapacity: 100,
+  chargingTime: "1 hour",
+  sunroof: true
+});
+
+const teslaModelS: ElectricCar = {
+  brand: "Tesla",
+  model: "Model S",
+  year: "2024",
+  batteryCapacity: 100,
+  chargingTime: "1 hour",
+};
